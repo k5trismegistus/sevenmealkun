3e662534d3429d113ec8e2d05788cdf119eb7b3c
keigo yamamoto <k5.trismegistus@gmail.com>
2018-02-08 21:02:41 +0900

    feat: Can order (Fixed date)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,65 @@
-const puppeteer = require('puppeteer');
+const setting = require('../config.json')
+const puppeteer = require('puppeteer')
+const Botkit = require('botkit')
 
-(async () => {
+console.log(setting)
+
+const controller = Botkit.slackbot({
+  debug: false
+})
+
+controller.spawn({
+  token: setting.slackToken
+}).startRTM((err, bot, payload) => {
+  if (err) {
+    console.log('Error: Cannot to Slack')
+    process.exit(1)
+  }
+})
+
+controller.hears('menu', ['direct_message', 'mention', 'direct_mention'], async (bot, message) => {
+  bot.reply(message, 'Please be patient...')
+  const r = await getMenuToShow()
+  bot.reply(message, r.menuName)
+  bot.reply(message, r.menuImage)
+  bot.reply(message, r.menuLink)
+})
+
+controller.hears('order', ['direct_message', 'direct_mention', 'mention'], async (bot, message) => {
   const browser = await puppeteer.launch({
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox'
     ]
   })
-
   const page = await browser.newPage()
+
+  bot.reply(message, 'Please be patient...')
+  await order(page)
+  bot.reply(message, '注文しました')
+})
+
+controller.hears('mode', ['direct_message', 'mention'], (bot, message) => {
+  bot.reply(message, '雑穀米 ごはん少なめ')
+})
+
+const getMenuToShow = async () => {
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  })
+  const page = await browser.newPage()
+
+  const rawMenus = await getRawMenus(page)
+  const tommorowMenu = tomorrowMenu(rawMenus)
+
+  browser.close()
+  return tommorowMenu[0]
+}
+
+const getRawMenus = async (page) => {
   await page.goto('https://7-11net.omni7.jp/basic/0501001003003000')
 
   await page.waitForSelector('.mod-shoppingContents_item')
@@ -23,8 +74,49 @@ const puppeteer = require('puppeteer');
       return { menuName, menuLink, menuImage }
     })
   })
+  return rawMenus
+}
 
-  console.log(rawMenus)
+const tomorrowMenu = (rawMenus) => {
+  let d = new Date()
+  d.setDate(d.getDate() + 2)
+  const tommorowMonth = d.getMonth() + 1
+  const tommorowDate = d.getDate()
 
-  browser.close()
-})()
+  const dateReg = new RegExp(`${tommorowMonth}月${tommorowDate}日`)
+  const riceReg = new RegExp('雑穀米')
+  const menu = rawMenus
+    .filter((rawMenu) => (dateReg.test(rawMenu.menuName)))
+    .filter((tomorrowMenu) => (riceReg.test(tomorrowMenu.menuName)))
+
+  return menu
+}
+
+const order = async (page) => {
+  await logIn(page)
+
+  const menu = await getMenuToShow()
+  page.goto(menu.menuLink)
+  try {
+    await page.waitForNavigation()
+    await page.$eval('input[title="カートに入れる"]', el => el.click())
+    await page.waitForNavigation()
+    await page.$eval('button[type="submit"]', el => el.click())
+    await page.waitForNavigation()
+    await page.$eval('a[class="linkBtn js-pressTwice"]', el => el.click())
+    await page.waitForNavigation()
+    await page.$eval('input[type="submit"]', el => el.click())
+    await page.waitForNavigation()
+  } catch (e) {
+    await page.screenshot({path: 'error.png'})
+  }
+}
+
+const logIn = async (page) => {
+  await page.goto('https://www.omni7.jp/account/login/')
+
+  await page.$eval('input[name="login"]', (el, mail) => { el.value = mail }, setting.omniSevenEmail)
+  await page.$eval('input[name="password"]', (el, password) => { el.value = password }, setting.omniSevenPassword)
+  await page.click('#loginBtn')
+  await page.waitForNavigation()
+}
